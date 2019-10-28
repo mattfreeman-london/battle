982e62f74668bc9f3978af45f8d9a938da9c8986
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-10-28 14:36:55 +0000

    add name of cat

--- app.rb
+++ app.rb
@@ -1,23 +1,5 @@
 require 'sinatra'
 
-get '/' do
-  "Hello World"
-end
-
-get '/secret' do
-  "bibbly bobbly boo"
-end
-
-get '/wizard' do
-  "you shall not pass!"
-end
-
-get '/blues' do
-  "everybody needs somebody to love"
-end
-
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-     <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  erb(:index)
 end
