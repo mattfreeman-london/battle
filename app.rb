6f0e188175dad0c15584751a262c8a7f92cddc80
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-10-29 16:31:21 +0000

    testing with capybara

--- app.rb
+++ app.rb
@@ -6,8 +6,18 @@ get '/random-cat' do
   erb(:index)
 end
 
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
+end
+
 get '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/cat-form' do
+  erb :cat_form
 end
